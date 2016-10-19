787ccd20def01c12ed6d941d54c3208984e9a0a9
Todd Enders <tenders64@gmail.com>
2016-10-19 15:08:03 -0700

    updated bugs

--- apps/languages/views.py
+++ apps/languages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
+from django.contrib import messages
 from .models import Language
 # Create your views here.
 def index (request):
@@ -12,10 +13,14 @@ def new (request):
 def create (request):
     if request.method == 'POST':
         result = Language.objects.validate_language(name=request.POST['name'])
+        print result
         if result[0]:
             return redirect(reverse('index'))
         else:
-            pass
+            print "+============"
+            for message in result[1]:
+                messages.error(request, message)
+            return redirect(reverse('new'))
             # we have errors we need to get back out to the 'new' template
     else:
         return redirect(reverse('index'))
@@ -32,10 +37,17 @@ def edit (request, id):
 
 def update (request, id):
     if request.method == 'POST':
-        # How do we update a language object?
-        pass
+        if len(request.POST['name'])<2:
+            messages.error(request, 'Name must be at least 2 characters')
+            return redirect(reverse('edit', kwargs={'id':id}))
+        else:
+            language = Language.objects.get(id=id)
+            language.name = request.POST['name']
+            print language.name
+            language.save()
+            return redirect(reverse('show', kwargs={'id':id}))
     else:
-        return redirect(reverse('edit')) #Gosh, I want to send them back to the specific language's edit page!
+        return redirect(reverse('edit', kwargs={'id':id})) #Gosh, I want to send them back to the specific language's edit page!
 
 def delete (request, id):
     pass
